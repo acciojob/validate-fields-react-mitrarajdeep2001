1aee0a95a896d5d2be5bd4767d07bcd889ad0835
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-06-23 10:35:15 +0000

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [showError, setShowError] = useState(false)
+  function submitForm(e) {
+    e.preventDefault()
+    if (!(username && password)) {
+      setShowError(true)
+    }
+    else setShowError(false)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <form>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input id="username" type="text" onChange={(e) => setUsername(e.target.value)}/>
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input id="password" type="password" onChange={(e) => setPassword(e.target.value)}/>
+          </div>
+          {showError && <p id="errorMessage">Both username and password are required.</p>}
+          <input type="submit" onClick={submitForm}/>
+        </form>
     </div>
   )
 }
